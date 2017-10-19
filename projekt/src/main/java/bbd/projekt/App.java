f0dddffdaecc2674f176af858d502ea018c2d3ca
WojciechSerafin <wojciech.serafin93@gmail.com>
2017-10-19 22:06:52 +0200

    Wrzucenie projektu Hello World!

---
+++ projekt/src/main/java/bbd/projekt/App.java
@@ -0,0 +1,13 @@
+package bbd.projekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
